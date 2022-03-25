22c6ee6f43b023ce705e2778379e6adaf994e396
dressalsl <dressalsl97@gmail.com>
2022-03-24 22:23:58 -0300

    remocao-comentario

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -4,15 +4,6 @@ class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        // body: Center(
-        //   child: Column(
-        //     mainAxisAlignment: MainAxisAlignment.start,
-        //     children: [
-        //       Image.asset("assets/images/logo1.png", width: 500, height: 350)
-        //     ],
-        //   ),
-        // ),
-        );
+    return Scaffold();
   }
 }
