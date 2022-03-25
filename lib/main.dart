df10207be80be296efeae953c95ea3beb7d81459
dressalsl <dressalsl97@gmail.com>
2022-03-24 22:22:37 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_sigma/views/app_widget.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
---
+++ lib/views/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // body: Center(
+        //   child: Column(
+        //     mainAxisAlignment: MainAxisAlignment.start,
+        //     children: [
+        //       Image.asset("assets/images/logo1.png", width: 500, height: 350)
+        //     ],
+        //   ),
+        // ),
+        );
+  }
+}
