5b374ef0955b41cac5704c15f960c8e744aa07b8
dressalsl <dressalsl97@gmail.com>
2022-04-15 15:41:13 -0300

    tela-menu-v1

--- lib/views/app_widget.dart
+++ lib/views/app_widget.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
-import 'home_page.dart';
+import 'menu_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage()
-    );
+    return MaterialApp(
+        initialRoute: '/',
+        routes: {'/': (context) => MenuPage()},
+        debugShowCheckedModeBanner: false);
+    // ignore: prefer_const_constructors
   }
 }
---
+++ lib/views/menu_page.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class MenuPage extends StatefulWidget {
+  const MenuPage({Key? key}) : super(key: key);
+
+  @override
+  _MenuPageState createState() => _MenuPageState();
+}
+
+class _MenuPageState extends State<MenuPage> {
+  Card cardItem(String title, int index) {
+    return Card(
+      elevation: 2,
+      margin: const EdgeInsets.all(3),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: const Color(0xff0303ff),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.white,
+              blurRadius: 3,
+              offset: Offset(2, 2),
+            )
+          ],
+        ),
+        child: InkWell(
+          onTap: () {
+            if (index == 0) {
+              //1.item
+            }
+            if (index == 1) {
+              //2.item
+            }
+            if (index == 2) {
+              //3.item
+            }
+            if (index == 3) {
+              //4.item
+            }
+            if (index == 4) {
+              //5.item
+            }
+            if (index == 5) {
+              //6.item
+            }
+          },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            verticalDirection: VerticalDirection.down,
+            children: [
+              const SizedBox(height: 50),
+              const SizedBox(height: 20),
+              Center(
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                      fontSize: 19,
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal),
+                ),
+              ),
+              const SizedBox(height: 10),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          const SizedBox(height: 100),
+          Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 2,
+              padding: const EdgeInsets.all(2),
+              children: [
+                cardItem("Selecionar Disciplinas", 0),
+                cardItem("Descrição do Curso", 1),
+                cardItem("Fórum", 2),
+                cardItem("Tela de Indicação", 3),
+                cardItem("Grade Atual", 4),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
